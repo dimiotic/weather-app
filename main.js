fb915e65517830450737d3dff03d8e3e0d9da48d
Dimiotic <dimiotic@gmail.com>
2023-01-22 09:27:21 +0100

    2.0

--- main.js
+++ main.js
@@ -6,55 +6,67 @@ const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity')
 
 
+function removeCard() {
+  const prevCard =  document.querySelector('.card')
+  if (prevCard) prevCard.remove();
+}
+
+
+function showError(errorMessage) {
+  const html = `<div class="card">${errorMessage}</div>`
+  header.insertAdjacentHTML('afterend', html);
+}
+
+function showCard({name, country, temp, condition}) {
+  const html = 
+  `<div class="card">
+      <h2 class="card-city">
+        ${name} <span>${country}</span> 
+      </h2>
+
+      <div class="card-weather">
+        <div class="card-value">${temp}<sup>°c</sup></div>
+        <img class="card-img" src="./img/cloudy.png" alt="weather img">
+      </div>    
+
+      <div class="description">${condition}</div>
+    </div>`
+
+
+  header.insertAdjacentHTML('afterend', html);
+}
+
+async function getWeather(city) {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
 
 // Слухаємо відправку форми
 
-form.onsubmit = function (e) {
+form.onsubmit = async function (e) {
   e.preventDefault()
 
-
   let city = input.value.trim();
-  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
 
+  const data = await getWeather(city)
 
-  fetch(url).then((response) => {
-    return response.json()
-  }).then((data) => {
+  if (data.error) {
+    removeCard();
+    showError(data.error.message)
+  } else {
+    removeCard()
 
-    if (data.error) {
-      const prevCard =  document.querySelector('.card')
-      if (prevCard) prevCard.remove();
-
-      const html = `<div class="card">${data.error.message}</div>`
-      header.insertAdjacentHTML('afterend', html);
-    } else {
-    // Display on card
-
-    const html = 
-    `<div class="card">
-        <h2 class="card-city">
-          ${data.location.name} <span>${data.location.country}</span> 
-        </h2>
-
-        <div class="card-weather">
-          <div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
-          <img class="card-img" src="./img/cloudy.png" alt="weather img">
-        </div>    
-
-        <div class="description">${data.current.condition.text}</div>
-      </div>`
-
-
-      header.insertAdjacentHTML('afterend', html);
+    const weatherData = {
+      name: data.location.name,
+      country: data.location.country,
+      temp: data.current.temp_c,
+      condition: data.current.condition.text
     }
 
-    console.log(data);
-    console.log(data.location.name);
-    console.log(data.location.country);
-    console.log(data.current.temp_c);
-    console.log(data.current.condition.text);
+    showCard(weatherData)
+  }
 
-   
-  })
 }
 
